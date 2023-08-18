3823404a03bc0957415a8016f58812d12b3ad013
Ruben Restrepo <ruben@stateful.com>
2023-08-18 12:22:25 -0500

    testing

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const reverseWords = (subject, verb, object) => [subject, verb, object].reverse().join('🤗')
+const reverseWords = (subject, verb, object) => [subject, verb, object].reverse().join('😇')
 
 module.exports = {
     reverseWords
